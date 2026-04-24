c872d5edb9de611868be56a44a37a0d0de03305a
Jayden Sakow <jaydensakow@gmail.com>
2026-04-23 20:47:27 -0700

    After Day 1

--- Assets/Scripts/Corgi.cs
+++ Assets/Scripts/Corgi.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class Corgi : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private SpriteRenderer corgieSpriteRenderer;
+
+    public void Awake()
     {
+        corgieSpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public void Move(Vector2 direction)
+    {
+        FaceCorrectDirection(direction);
         
+        Vector2 moveAmount = direction * GameParameters.CorgiMoveSpeed * Time.deltaTime;
+        corgieSpriteRenderer.transform.Translate(moveAmount);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FaceCorrectDirection(Vector2 direction)
     {
+        if (direction.x > 0)
+        {
+            corgieSpriteRenderer.flipX = false;
+        }
         
+        else if (direction.x < 0)
+        {
+            corgieSpriteRenderer.flipX = true;
+        }
     }
 }
